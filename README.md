5b1be900194c4276c1f2d5f1275d3d91e1001c26
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-03-08 19:28:44 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+# TarkovDLSS45
+This prepatcher lets you use DLSS 4.5 in SPT/Fika.
+
+## Installation
+1. Copy 'nvngx_dlss.dll' from your live 'EscapeFromTarkov_Data\Plugins\x86_64' folder
+    - The 4.5 .dll can also be found [here](https://www.techpowerup.com/download/nvidia-dlss-dll/)
+2. Paste the .dll in your SPT 'EscapeFromTarkov_Data\Plugins\x86_64' folder
+3. Extract the downloaded prepatcher
+4. Change to any of the new presets in game
+
+## Notes
+Before installing, make sure that you set DLSS to >preset J, or your game will fail to load. The plugin removes old presets as they are no longer functional with DLSS 4.5.
+Setting it to any preset before J will default back to K automatically, this happens even without this mod when using the new DLSS .dll.
+
+Don't forget to disable overrides in the drivers.
---
+++ TarkovDLSS45/TarkovDLSS45.cs
@@ -0,0 +1,44 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TarkovDLSS45
+{
+    public static class TarkovDLSS45
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Unity.Postprocessing.Runtime.dll" };
+
+        public static void Patch(ref AssemblyDefinition assembly)
+        {
+            var module = assembly.MainModule;
+
+            var enumType = module.Types.First(t => t.FullName == "EDLSSPreset");
+            var fieldsToRemove = enumType.Fields
+            .Where(f => f.Name != "value__" &&
+                        f.Name != "J" &&
+                        f.Name != "K")
+            .ToArray();
+
+            foreach (var field in fieldsToRemove)
+            {
+                enumType.Fields.Remove(field);
+            }
+
+            AddNewEnumField(enumType, "L", 12);
+            AddNewEnumField(enumType, "M", 13);
+        }
+
+        private static void AddNewEnumField(TypeDefinition enumType, string name, int value)
+        {
+            const FieldAttributes fieldAttributes = FieldAttributes.Static | FieldAttributes.Literal
+                | FieldAttributes.Public | FieldAttributes.HasDefault;
+
+            var field = new FieldDefinition(name, fieldAttributes, enumType)
+            {
+                Constant = value
+            };
+
+            enumType.Fields.Add(field);
+        }
+    }
+}
